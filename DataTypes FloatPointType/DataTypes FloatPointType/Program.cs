555f0b214a1952e6f3d92330d3372edf5b7da42a
karim khafaji <k.khafaji@hotmail.com>
2017-10-10 21:45:07 +0200

    DataTypes FloatPointType
    this is the first Data Types Float Point Type

---
+++ DataTypes FloatPointType/DataTypes FloatPointType/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes_FloatPointType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flMin = float.MinValue;
+            float flMax = float.MaxValue;
+
+            Console.WriteLine("Min Value = {0} max Value = {1}", flMin, flMax);
+            Console.ReadKey();
+
+        }
+    }
+}
