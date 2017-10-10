1c78f809d1168a04659fcce0a53b7cf10ec4d06d
karim khafaji <k.khafaji@hotmail.com>
2017-10-10 22:05:25 +0200

    WhyTheyUseTypeDecimalForFinancial and banking
    here is the answer

---
+++ WhyTheyUseTypeDecimalForFinancial and banking/WhyTheyUseTypeDecimalForFinancial and banking/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhyTheyUseTypeDecimalForFinancial_and_banking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flVar = 1F / 3;
+            double dlVar = 1D / 3;
+            decimal dcVar = 1M / 3;
+
+            Console.WriteLine("\nfloat Value = {0}\ndouble Value = {1}\ndecimal Value = {2}", flVar, dlVar,dcVar);
+            Console.ReadKey();
+        }
+    }
+}
