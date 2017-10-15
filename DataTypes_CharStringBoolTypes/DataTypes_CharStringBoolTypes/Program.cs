02574f58a674473bef4ce6d481f5b823f1db7d22
karim khafaji <k.khafaji@hotmail.com>
2017-10-16 00:03:28 +0200

    Data Types Char String Bool Types
    Char String Bool Types

---
+++ DataTypes_CharStringBoolTypes/DataTypes_CharStringBoolTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes_CharStringBoolTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int charMin = char.MinValue;
+            int charMax = char.MaxValue;
+
+            char myVar = 'G';
+            int myVarValue = 'G';
+
+            char myVarvalue2 = Convert.ToChar(myVarValue);
+            Console.WriteLine("Min char = {0} Max char = {1} My character = {2} My Variable Nummber = {3} My Conversion Value = {4}", charMin, charMax, myVar, myVarValue, myVarvalue2);
+
+            String myString = "this is my Variable";
+            Console.WriteLine("\nmyString is {0}", myString);
+
+            bool myBool = true;
+            Console.WriteLine("my boolean value is {0}", myBool);
+
+            Console.ReadKey();
+        }
+    }
+}
