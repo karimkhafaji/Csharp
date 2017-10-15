3c6f369df01bb9863ca1e1ac14fb5a83acfd4ba6
karim khafaji <k.khafaji@hotmail.com>
2017-10-16 01:05:29 +0200

    Literal Characters
    Overview of Literal Characters

---
+++ Literal Characters/Literal Characters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literal_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int myVar = -100;
+            uint myUVar = 1000U;
+            long myLVar = 1000;
+            ulong myULVar = 1000UL;
+
+            float myFlVar = 1.5F;
+            double myDVar = 100.5D;
+            decimal myDECVar = 100.5M;
+
+
+            Console.WriteLine("{0} {1}{2}{3}", myVar, myUVar, myLVar, myULVar);
+
+            Console.WriteLine($"\n {myFlVar} \n {myDVar} \n {myDECVar}");
+            Console.ReadKey();
+        }
+    }
+}
