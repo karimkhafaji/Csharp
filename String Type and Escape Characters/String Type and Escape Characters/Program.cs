434ff1b093de98913e430af850878614812a28de
karim khafaji <k.khafaji@hotmail.com>
2017-10-16 21:00:04 +0200

    String Type and Escape Characters
    String Type and Escape Characters + escape sequences

---
+++ String Type and Escape Characters/String Type and Escape Characters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Type_and_Escape_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //\' \" \\ \0 \a \b \n \r \t \v \f
+
+            string myString = "Karim said\n \"This is a good start\"\a and I think...";
+
+            Console.WriteLine("{0}", myString);
+            Console.ReadKey();
+
+        }
+    }
+}
