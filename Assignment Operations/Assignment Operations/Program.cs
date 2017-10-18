dc6713fa5f414db9e97d3edc5b00100e900780db
karim khafaji <k.khafaji@hotmail.com>
2017-10-18 20:04:01 +0200

    Assignment Operations
    = , += , -= , *= , /= , %=

---
+++ Assignment Operations/Assignment Operations/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // = , += , -= , *= , /= , %=
+
+            int myVar1 = 7, myVar2 = 3;
+            // myVar1 = myVar2;
+            // myVar2 = myVar1;
+
+            // myVar1 += myVar2; //myVar1 = myVar1 + myVar2
+            //myVar1 -= myVar2;  //myVar1 = myVar1 - myVar2
+            myVar1 %= myVar2;    //myVar1 = myVar1 % myVar2
+
+
+
+            Console.WriteLine("{0}", myVar1);
+            Console.ReadKey();
+        }
+    }
+}
