4492f58ef82144480cee66cb78e28089c0f60b1d
karim khafaji <k.khafaji@hotmail.com>
2017-10-18 21:04:04 +0200

    Boolean Operators
    Boolean Operators
    && || True or False

---
+++ Boolean Operators/Boolean Operators/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Boolean comparison == , != , < , > , <= , >=
+            //Boolean conditions && , ||
+
+
+            Boolean myResult;
+            int myVari1 = 5,myVari2 = 7;
+            //myResult = myVari1 != 6;
+
+            //myResult = (myVari1 == 5) && (myVari2 == 7);                                                 //Truth Table  & ||
+            myResult = (myVari1 >= 6) || (myVari2 >= 7);                                                   // F    F   -- F  F
+                                                                                                           // F    T   -- F  T
+                                                                                                           // T    T   -- T  T
+                                                                                                           // T    F   -- F  T
+
+            Console.WriteLine("That answar is = {0}", myResult);
+            Console.ReadKey();
+
+
+        }
+    }
+}
