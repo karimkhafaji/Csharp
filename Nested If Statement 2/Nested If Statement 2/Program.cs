ed9a50868695bd8f452e74090fd4b7539ec9e994
karim khafaji <k.khafaji@hotmail.com>
2017-10-19 00:21:45 +0200

    Nested If Statement 2
    Nested If Statement Part 2

---
+++ Nested If Statement 2/Nested If Statement 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_If_Statement_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myvar1 = 4, myvar2 = 6;
+
+            if (myvar1 <= 3 && myvar2 >= 5)
+            {
+                Console.Write("condition one is right");
+            }
+            else if  (myvar1 <= 4 && myvar2 >= 6)
+            {
+                Console.WriteLine("condition two is right");
+            }
+            else if (myvar2 > 5)
+            {
+                Console.WriteLine("condition three is right");
+
+            }
+            else
+            {
+                Console.WriteLine("nothing right of the conditions");
+            }
+            Console.ReadKey();
+        }
+    }
+}
