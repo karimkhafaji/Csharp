77b4003a1f82bf29468ae47f5c443d11ac767103
karim khafaji <k.khafaji@hotmail.com>
2017-10-18 18:55:18 +0200

    Increments & Decrements Operations
    Increments & Decrements Operations

---
+++ Increments - Decrements Operations/Increments - Decrements Operations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Increments___Decrements_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int myVar1 = 5;
+            int myVar2 = 9;
+            int temp1 = ++myVar1;
+            int temp2 = --myVar2;
+            Console.WriteLine("{0}\n{1}", temp1, temp2);
+            Console.ReadKey();
+
+        }
+    }
+}
