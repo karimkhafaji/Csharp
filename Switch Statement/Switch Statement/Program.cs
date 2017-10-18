ec8c58ae34648231405c029588005bdfc70ca7f8
karim khafaji <k.khafaji@hotmail.com>
2017-10-19 00:57:58 +0200

    Switch Statement
    Switch , case : break and default : break

---
+++ Switch Statement/Switch Statement/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string myanimal = "cat";
+
+            //switch (myanimal)
+            //{
+            //    case "canary":
+            //        Console.WriteLine("my animal is canary");
+            //        break;
+            //    case "camel":
+            //        Console.WriteLine("my animal is camel");
+            //        break;
+            //    case "cat":
+            //        Console.WriteLine("my animal is cat");
+            //        break;
+            //    default:
+            //        Console.WriteLine("no one");
+            //        break;
+
+            //}
+            //Console.ReadKey();
+
+
+            int myvar = 15;
+
+            switch (myvar)
+            {
+                case 10:
+                   Console.WriteLine("my variable number is 10");
+                    break;
+                case 7:
+                    Console.WriteLine("my variable number is 7");
+                    break;
+                case 4:
+                    Console.WriteLine("my variable number is 4");
+                   break;
+               default:
+                   Console.WriteLine("no one");
+                   break;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
