458b3ac9447f96589cf377dace6c1f77edbd233e
karim khafaji <k.khafaji@hotmail.com>
2017-10-20 00:28:10 +0200

    Nested Loop
    Nested Loop

---
+++ Nested Loop/Nested Loop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 3; i++) 
+            {
+                Console.WriteLine("Hello Karim");
+                for (int j = 0; j < 2; j++) 
+                {
+                    Console.WriteLine("\t Hello World");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
