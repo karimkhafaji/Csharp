af01e32f376d6cd375b57c4520e9383179f2e812
karim khafaji <k.khafaji@hotmail.com>
2017-10-20 00:00:04 +0200

    ForLoop Function
    ForLoop Function

---
+++ ForLoop Function/ForLoop Function/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mySalary = 1000, total = 0;
+
+            for (int i = 0; i < 12; i++)
+            {
+                total += mySalary;
+            }
+            Console.WriteLine("My Salary for 12 Months is {0}", total);
+            Console.ReadKey();
+        }
+    }
+}
