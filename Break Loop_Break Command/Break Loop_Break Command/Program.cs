381dc07d8bb6fabf913304d33ad647276e0ab928
karim khafaji <k.khafaji@hotmail.com>
2017-10-20 01:11:27 +0200

    Break Loop_Break Command
    Break Loop with Break Command

---
+++ Break Loop_Break Command/Break Loop_Break Command/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_Loop_Break_Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Hello Karim");
+                if (i > 3)
+                    break;
+                for (int j = 0; j <= 4; j++)
+                {
+                    Console.WriteLine("\t Hello World");
+                }
+                Console.WriteLine("\n");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
