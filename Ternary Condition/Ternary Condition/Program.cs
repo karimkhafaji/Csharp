7aa32d76742c1748dc275640236a6f631905ce2a
karim khafaji <k.khafaji@hotmail.com>
2017-10-19 21:55:40 +0200

    Ternary Condition
    Ternary Condition

---
+++ Ternary Condition/Ternary Condition/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ternary_Condition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int myvar = 12;
+
+            //string myResult = (myvar < 10) ? "Less than 10" : "greater than 10";
+
+            //Console.WriteLine("My Result is {0}", myResult);
+            //Console.ReadKey();
+
+
+
+            string myName = "KaRiM", myResult;
+
+            myResult = (myName.Equals("karim")) ? "Yes" : "No";
+            Console.WriteLine("The Answar is {0}", myResult);
+            Console.ReadKey();
+
+        }
+    }
+}
