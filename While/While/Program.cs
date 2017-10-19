e7e2ec1c91136617492e907d3c02f5cc7fe1f594
karim khafaji <k.khafaji@hotmail.com>
2017-10-19 23:02:11 +0200

    While
    While

---
+++ While/While/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            double mySalary = 1020.30, total = 0.0;
+
+            while (i < 15)
+            {
+                total += mySalary;
+                i++;
+            }
+            Console.WriteLine("My Salary is {0}", total);
+            Console.ReadKey();
+        }
+    }
+}
