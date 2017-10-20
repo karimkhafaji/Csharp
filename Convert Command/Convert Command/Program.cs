336921ccecb0e2998fa0696cc215b38279d3489a
karim khafaji <k.khafaji@hotmail.com>
2017-10-21 01:47:52 +0200

    Convert Command
    Convert Command

---
+++ Convert Command/Convert Command/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double myDoublevar = 2500.45D;
+            string myStringvar, mySubStringvar;
+
+            myStringvar = Convert.ToString(myDoublevar);
+            mySubStringvar = myStringvar.Substring(0, 2);
+
+            Console.WriteLine($"my variable is {myDoublevar} \n");
+            Console.WriteLine("my string variable is {0}\n", myStringvar);
+            Console.WriteLine("my string variable is {0} \n", mySubStringvar);
+            Console.ReadKey();
+        }
+    }
+}
