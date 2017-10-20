646fb26e0296d16e0f48741b6d2fb5a1355e44e5
karim khafaji <k.khafaji@hotmail.com>
2017-10-21 01:08:31 +0200

    Checked and Unchecked
    Checked and Unchecked

---
+++ Checked and Unchecked/Checked and Unchecked/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checked_and_Unchecked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // type1 = (type1)type2           the rule              //casting
+
+
+                                                                   //overflow
+            byte mySbyte;                  //byte value            //0 - 255         
+            int myInt = 350;               //integer               //‭000101011110‬
+            // mySbyte = myInt;                                    //‭000011111111‬     +
+            mySbyte = checked((byte)myInt);                                 //000001011110
+            Console.WriteLine($"My Variable int = {myInt} " + "\n");
+            Console.WriteLine($"My Byte Bariable = {mySbyte}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
