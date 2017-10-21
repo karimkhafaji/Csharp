1af589522826dddac1ff7d56075cbf244f536474
karim khafaji <k.khafaji@hotmail.com>
2017-10-22 00:18:58 +0200

    Input Methods (ReadLine,Read)
    Input Methods In C#

---
+++ Input Methods In C Sharp/Input Methods In C Sharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input_Methods_In_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            int myVar1, myVar2;
+            Console.WriteLine("Please Enter Your Name \n");
+            myName = Console.ReadLine();
+            Console.WriteLine("\nPlease Enter Your First Number\n");
+            myVar1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nPlease Enter Your Entry");
+            myVar2 = Console.Read();
+            
+            Console.WriteLine($"\nHello {myName}");
+            Console.WriteLine($"\nYou Entered The Number {myVar1}");
+            Console.WriteLine($"\nYour Entry is {myVar2}");
+            Console.ReadKey();
+
+        }
+    }
+}
