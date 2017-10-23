68d87f0d8ad943e6282ddae3acd3a5a260742043
karim khafaji <k.khafaji@hotmail.com>
2017-10-23 19:30:17 +0200

    Array Of String and Array Length
    Array Of String and Array Length

---
+++ Array Of String and Array Length/Array Of String and Array Length/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Of_String_and_Array_Length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myFullName = { "KaRiM", "Ahmad", "Khafaji" };
+
+            int myArrayLength = myFullName.Length;
+
+            for (int i = 0; i < myArrayLength; i++)
+            {
+                Console.Write($"{myFullName[i]}" + " ");
+            }
+            Console.WriteLine("\n\nMy Array Length is "+ $"{myArrayLength}");
+            Console.ReadKey();
+        }
+    }
+}
