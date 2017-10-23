947376591932ef0037868718b1677f04b72b2a98
karim khafaji <k.khafaji@hotmail.com>
2017-10-23 23:57:40 +0200

    String Manipulation Operations + Challenge 25
    String Manipulation Operations + Challenge 25

---
+++ String Manipulation Operations/String Manipulation Operations/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Manipulation_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Karim Khafaji";
+            char[] myCharArray = myString.ToCharArray();
+
+            Console.WriteLine($"The String Length is {myString.Length}\n");
+
+            foreach (char mychar in myCharArray)
+            {
+
+                Console.WriteLine($"{mychar}");
+            }
+            Console.ReadKey();
+
+            //Define a string variable than assign "I am Smart" change the substring "i am "
+            // with "He is" ==> "He is Smart"
+
+            //string myString = "I am Smart";
+            //myString = myString.Substring(4);
+            //myString = "He is" + myString;
+            //char[] myChararray = myString.ToCharArray();
+
+            //foreach (char temp in myString)
+            //{
+            //    Console.WriteLine($"{temp}");
+
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
