b5f408247947fb86ccc3a3fa4315beef5c59c81a
karim khafaji <k.khafaji@hotmail.com>
2017-10-24 18:43:20 +0200

    First Function in C#
    First Function in C#

---
+++ First Function in Csharp/First Function in Csharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Function_in_Csharp
+{
+    class Program
+    {
+        static void printName()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+
+        static void Main(string[] args)
+        {
+            printName();
+            Console.ReadKey();
+
+        }
+    }
+}
