453889675a3a90daa2cee8ff84e16a2d09985281
karim khafaji <k.khafaji@hotmail.com>
2017-10-24 02:59:47 +0200

    ToUpper Vs ToLower , Trim Functions
    ToUpper Vs ToLower , Trim Functions

---
+++ ToUpper Vs ToLower , Trim Functions/ToUpper Vs ToLower , Trim Functions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToUpper_Vs_ToLower___Trim_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string myString, myString2;
+
+            //myString2 = myString.ToUpper();
+            //Console.WriteLine($"{myString2}");
+            //myString2 = myString.Tolower();
+            //Console.WriteLine($"{myString2}");
+
+            char[] mytrimChar = { 'h', 'e', 'i', ' ' };
+            Console.WriteLine("Please Enter Your String");
+            myString = Console.ReadLine();
+            myString = myString.ToLower();
+            myString2 = myString.Trim(mytrimChar);
+            Console.WriteLine($"{myString2}");
+
+            Console.ReadKey();
+        }
+    }
+}
