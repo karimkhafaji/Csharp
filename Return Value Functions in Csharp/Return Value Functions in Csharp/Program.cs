15599f26d22a2f1bc51c6c9d1fb8b04137f4da95
karim khafaji <k.khafaji@hotmail.com>
2017-10-24 19:52:10 +0200

    Arrguments and Parameters
    Return Value Functions in C#

---
+++ Return Value Functions in Csharp/Return Value Functions in Csharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Return_Value_Functions_in_Csharp
+{
+    class Program
+    {
+        //static void addNumbers(int myFirstNumber, int mySecondNumber)
+        //{
+        //    int temp = myFirstNumber + mySecondNumber;
+        //    Console.WriteLine($"{temp}");
+
+        //}
+
+        static int addNumbers2(int myFirstNumber, int mySecondNumber)
+            {
+            int temp = myFirstNumber + mySecondNumber;
+            return temp;
+
+            }
+
+        static void Main(string[] args)
+        {
+            int myVar1 = 5, myVar2 = 3, temp;
+
+            temp = addNumbers2(myVar1,myVar2);
+
+            //addNumbers(myVar1, myVar2);
+
+            Console.WriteLine($"{temp}");
+            Console.ReadKey();
+        }
+    }
+}
