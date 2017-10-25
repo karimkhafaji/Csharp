6671c0bfe6ed7402e038f2daf198f99ff73e1fa8
karim khafaji <k.khafaji@hotmail.com>
2017-10-25 11:19:50 +0200

    Variables Scope
    Variables Scope

---
+++ Variables Scope/Variables Scope/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_Scope
+{
+    class Program
+    {
+        static string myString2;
+        static void printMyString()
+        {
+            string myString = "My String inside the printMyString Method ";
+            Console.WriteLine($"{myString}");
+            Console.WriteLine($"\t{ myString2}");
+
+        }
+
+        static void Main(string[] args)
+        {
+            string myString = "My String inside the Main Method";
+            myString2 = "Global myString variable";
+            printMyString();
+
+            Console.WriteLine($"\n{myString}");
+            Console.WriteLine($"\t{ myString2}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
