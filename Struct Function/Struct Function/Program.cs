5189dfc38c44dff9493ccd7f2c79c5bcc9294b90
karim khafaji <k.khafaji@hotmail.com>
2017-10-25 14:53:55 +0200

    Struct Function
    Struct Function

---
+++ Struct Function/Struct Function/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Function
+{
+    class Program
+    {
+            struct FullName
+            {
+            public string myFirstName, myLastName;
+            public string printName() => myFirstName + " " + myLastName;
+            }
+        static void Main(string[] args)
+        {
+            FullName myFullname;
+            myFullname.myFirstName = "Karim";
+            myFullname.myLastName = "Khafaji";
+            //Console.Write($"{myFullname.myFirstName} {myFullname.myLastName}");
+            Console.Write(myFullname.printName());
+            Console.ReadKey();
+        }
+    }
+}
