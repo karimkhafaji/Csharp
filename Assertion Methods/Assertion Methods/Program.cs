2579d3fb982c953f08b97a6364883b177f384bcf
karim khafaji <k.khafaji@hotmail.com>
2017-10-27 16:32:35 +0200

    Assertion Methods
    Assertion Methods

---
+++ Assertion Methods/Assertion Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Diagnostics;
+
+namespace Assertion_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"Enter a Value");
+            int i = Convert.ToInt32(ReadLine());
+            if (i >= 100)
+            {
+                WriteLine($"{i}");
+            }
+            else
+            {
+                Debug.WriteLine("We are Here");
+                Debug.Assert(i >= 100, "Short", "Long");
+                Trace.Assert(i >= 100, "Short", "Long");
+
+            }
+            WriteLine("Done ");
+
+            ReadKey();
+
+        }
+    }
+}
