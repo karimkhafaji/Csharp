420729ed3d525a0b57c4a6a203782233e282e0be
karim khafaji <k.khafaji@hotmail.com>
2017-10-30 00:04:22 +0100

    Nullable Types
    Nullable Types

---
+++ Nullable Types/Nullable Types/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Nullable_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string myString = null;         //only string type accept the value null without "?" Question mark
+            int? myVariable = null;
+            double? myDouble = null;
+            WriteLine(myString);
+            WriteLine(myVariable);
+            WriteLine(myDouble);
+            ReadKey();
+        }
+    }
+}
