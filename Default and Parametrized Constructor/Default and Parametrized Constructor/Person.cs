fbeccdae106cc3e91e96262703204fa753de22e0
karim khafaji <k.khafaji@hotmail.com>
2017-11-03 22:24:56 +0100

    Default & Parametrized Constructor + accessing member variables + using kw (this)
    Default & Parametrized Constructor + accessing member variables + using kw (this)

---
+++ Default and Parametrized Constructor/Default and Parametrized Constructor/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Default_and_Parametrized_Constructor
+{
+    class Person
+    {
+        //Member Variables
+        string name;
+        int age;
+
+
+        public void PrintValues()
+        {
+            WriteLine($"{name} , {age}");
+        }
+
+        //Deafult
+        public Person()
+        {
+            name = "Karim Khafaji";
+            age = 25;
+
+        }
+        //Parametrized Constructor
+        public Person(string name)
+        {
+            this.name = name;
+            age = 24;
+
+        }
+        //Parametrized Constructor
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+
+        }
+
+    }
+}
---
+++ Default and Parametrized Constructor/Default and Parametrized Constructor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Default_and_Parametrized_Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String myName = "Monier Sammo", myName2 = "Majid Dabl";
+            int age2 = 26;
+
+            Person myObj = new Person();
+            myObj.PrintValues();
+
+            WriteLine();
+            Person myObj2 = new Person(myName, age2);
+            myObj2.PrintValues();
+
+            WriteLine();
+            Person myObj3 = new Person(myName2);
+            myObj3.PrintValues();
+
+            ReadKey();
+        }
+    }
+}
