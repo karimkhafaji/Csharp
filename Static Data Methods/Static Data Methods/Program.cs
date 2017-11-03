004c7775f0d1be11364e08b0af73349a8567a1c0
karim khafaji <k.khafaji@hotmail.com>
2017-11-03 23:18:00 +0100

    Static Data Methods
    Static Data Methods

---
+++ Static Data Methods/Static Data Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Data_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Static_Data_and_Methods s1 = new Static_Data_and_Methods(50);
+            Static_Data_and_Methods s2 = new Static_Data_and_Methods(100);
+
+            // Print the current interest rate.
+            Console.WriteLine("Interest Rate is: {0}", Static_Data_and_Methods.GetValue());
+            Static_Data_and_Methods.SetInterestRate(0.7);
+
+            //Even If we make new object, this does NOT ’reset’ the interest rate.
+            Static_Data_and_Methods s3 = new Static_Data_and_Methods(2000);
+            Console.WriteLine("Interest Rate is: {0}", Static_Data_and_Methods.GetValue());
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Static Data Methods/Static Data Methods/Static_Data_and_Methods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Data_Methods
+{
+    class Static_Data_and_Methods
+    {
+        // Static Data at Instance-level.
+        public double currBalance;
+
+        // A static point of data.
+        public static double currInterestRate;
+
+        //Parameterized Constructor
+        public Static_Data_and_Methods(double balance)
+        {
+            currBalance = balance;
+
+        }
+
+        static Static_Data_and_Methods()
+        {
+            currInterestRate = 0.5;
+        }
+        public static void SetInterestRate(double newRate)
+        { currInterestRate = newRate; }
+
+        public static double GetValue()
+        { return currInterestRate; }
+
+
+    }
+}
