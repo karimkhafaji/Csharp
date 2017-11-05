eefddad48a377885f48395e410e0d247f66835f6
karim khafaji <k.khafaji@hotmail.com>
2017-11-05 22:34:06 +0100

    Static Classes
    Static Classes

---
+++ Static Classes/Static Classes/Employees1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace Static_Classes
+{
+    static class Employees1
+    {
+        static public string name = "Karim Khafaji";
+        static public int age = 24;
+
+        static public void PrintName()
+        {
+            WriteLine($"{name} , {age}");
+        }
+
+    }
+}
---
+++ Static Classes/Static Classes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Static_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employees1 myObj = new Employees1(); not allowed
+            Employees1.PrintName();
+            ReadKey();
+        }
+    }
+}
