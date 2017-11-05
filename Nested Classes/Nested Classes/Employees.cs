4065427ddba5e79ab318ba17222e243943ffb513
karim khafaji <k.khafaji@hotmail.com>
2017-11-05 17:27:38 +0100

    Nested Classes
    Nested Classes

---
+++ Nested Classes/Nested Classes/Employees.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Nested_Classes
+{
+    class Employees
+    {
+
+        public string fName = "Karim";
+        public string Address = "Eduard-Ellmauthaler Str";
+        public void PrintValues()
+        { WriteLine($"Employee Name is {fName} \nLiving at {Address}"); }
+
+
+        public class Departments
+        {
+            public string departmentName = "Administration";
+            public void PrintDepartmentName()
+            { WriteLine($"Working at {departmentName}"); }
+        }
+
+    }
+}
---
+++ Nested Classes/Nested Classes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Nested_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employees myEmployee = new Employees();
+            myEmployee.PrintValues();
+
+            Employees.Departments myDepart = new Employees.Departments();
+            myDepart.PrintDepartmentName();
+
+            ReadKey();
+        }
+    }
+}
