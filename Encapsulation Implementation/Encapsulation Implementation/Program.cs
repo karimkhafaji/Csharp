8e5d598bd74c12cdb6c617722e66103d4eb07c08
karim khafaji <k.khafaji@hotmail.com>
2017-11-08 23:12:22 +0100

    EncapsulationApp
    EncapsulationApp

---
+++ Encapsulation Implementation/Encapsulation Implementation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Karim Khafaji";
+            int age = 25;
+            Person myObj = new Person();
+            myObj.SetName(name);
+            myObj.SetAge(age);
+
+            Console.WriteLine("Name is " + myObj.GetName());
+            Console.WriteLine("Age is " + myObj.GetAge());
+            Console.ReadKey();
+
+        }
+    }
+}
