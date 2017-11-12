4e4c37c7b6b115e7c4570b028fc1dd4e141da78a
karim khafaji <k.khafaji@hotmail.com>
2017-11-12 22:01:42 +0100

    Abstract Classes and Methods
    Abstract Classes and Methods

---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Managers myMag = new Managers();
+            HR myHr = new HR();
+
+            myMag.name = "Karim";
+            myHr.name = "Majid";
+
+            myMag.PrintName();
+            myMag.PrintDepart();
+
+            Console.WriteLine();
+
+            myHr.PrintName();
+            myHr.PrintDepart();
+
+            Console.ReadKey();
+        }
+    }
+}
