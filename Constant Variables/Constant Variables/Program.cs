de8092803dd1beceda276084be8efb584b349e0d
karim khafaji <k.khafaji@hotmail.com>
2017-11-18 01:19:21 +0100

    ConstantVar + ReadOnlyFields + ReadOnlyFields Static access
    ConstantVar + ReadOnlyFields + ReadOnlyFields Static access

---
+++ Constant Variables/Constant Variables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant_Variables
+{
+    class Program
+    {
+        //public const double pi = 3.14;
+        static void Main(string[] args)
+        {
+            ReadOnlyFields myObj = new ReadOnlyFields();
+
+            Console.WriteLine($"{ReadOnlyFields.pi}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Constant Variables/Constant Variables/ReadOnlyFields.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant_Variables
+{
+    class ReadOnlyFields
+    {
+        public static readonly double pi;
+
+        static ReadOnlyFields()
+        {
+            pi = 3.14;
+        }
+    }
+}
