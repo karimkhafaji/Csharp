03a123ff6788b9477d5340b019621283edb320bf
karim khafaji <k.khafaji@hotmail.com>
2017-12-01 03:11:35 +0100

    Throwing a specific Exception
    Throwing a specific Exception

---
+++ Throwing a specific Exception/Throwing a specific Exception/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Throwing_a_specific_Exception
+{
+    class Program
+    {
+        public static void PrintSalary()
+        {
+            decimal Var1, Var2, totalTax;
+            try
+            {
+                WriteLine("Enter The Total Salary Before Cut Tax");
+                Var1 = Convert.ToDecimal(ReadLine());
+
+                WriteLine("Enter The Tax Percent");
+                Var2 = Convert.ToDecimal(ReadLine());
+                Var2 /= 100;
+
+                totalTax = Var1 * Var2;
+                WriteLine($"The Total Tax  Value is {totalTax}");
+            }
+            catch (FormatException ex1)
+            {
+                throw new Exception("Salary Format or Tax is not match, Try Again Later", ex1.InnerException);
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                PrintSalary();
+            }
+            catch (Exception myEx)
+            {
+                WriteLine(myEx.Message);
+            }
+
+            ReadKey();
+        }
+    }
+}
