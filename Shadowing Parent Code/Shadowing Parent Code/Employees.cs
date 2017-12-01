7c4ef22f18962b6b0b95506b73ad7dd85f5e22b2
karim khafaji <k.khafaji@hotmail.com>
2017-12-01 22:41:55 +0100

    Shadowing Parent Code
    Shadowing Parent Code

---
+++ Shadowing Parent Code/Shadowing Parent Code/Employees.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shadowing_Parent_Code
+{
+    //the abstract base class
+
+    abstract class Employees
+    {
+        public Employees(string name = "Karim Khafaji")
+        { EmpName = name; }
+
+        public string EmpName { get; set; }
+
+        public abstract void PrintName();
+    }
+
+
+    #region HumRe class
+    class HumRe : Employees
+    {
+        public HumRe() { }
+        public HumRe(string name) : base(name) { }
+        public override void PrintName()
+        {
+            Console.WriteLine("Print name {0} from the Hum Resources class", EmpName);
+        }
+    }
+    #endregion
+
+    #region HumRe
+    class Finance : HumRe
+    {
+        public new string EmpName { get; set; }
+
+        public new void PrintName()
+        {
+            Console.WriteLine("\nPrint a Finance Employee Name\n ");
+        }
+    }
+    #endregion
+}
---
+++ Shadowing Parent Code/Shadowing Parent Code/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shadowing_Parent_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Finance myFinanObj = new Finance();
+            myFinanObj.PrintName();
+
+            //Method of the Parent!
+            ((HumRe)myFinanObj).PrintName();
+            Console.ReadKey();
+
+        }
+    }
+}
